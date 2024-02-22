6e7f7d857afd14659dc264360d1cf5be304efa1e
kauan0204 <kauanduarteflauzino@gmail.com>
2024-02-22 17:22:24 -0300

    oi

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require ('express');
-const axios = require ('axios');
-const path = require ('path');
-const cors = require ('cors');
-const config = require ('./config.json');
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+const cors = require('cors');
+const config = require('./config.json');
 const apikey = config.apikey;
 
 const app = express();
@@ -10,23 +10,36 @@ app.listen(3000);
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(_dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const traducaoClima = {
-    'few clouds':'Poucas nuvens',
-    'scattered clouds': 'nuvens dispersas'
+    'few clouds': 'Poucas nuvens',
+    'scattered clouds': 'nuvens dispersas',
+    'overcast clouds': 'Nublado',
+    'clear sky': 'ceu limpo',
+    'broken clouds': 'nuvens separadas',
+    'moderate rain': 'chuva moderada',
+    'light rain': 'leve chuva',
+    'light intensity drizzle': 'garoa leve',
+    'shower rain': 'banho de chuva',
+    'light intensity shower rain': 'chuva media',
+    'haze': 'neblina',
+    'smoke':'fumaça'
+
+
+
 }
 
 
 app.get("/climatempo/:cidade", async (req, res) => {
 
-    const cidade = req.params.cidade;
-    try{
-        const response = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?q=${cidade}&appid=${APIkey}&units-metric`)
-        if(response.status== 200) {
+    const city = req.params.cidade;
 
-            const clima = traducaoClima[response.data.weather[0].description] 
-            || response.data.weather[0].description
+    try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`)
+        if (response.status === 200) {
+
+            const clima = traducaoClima[response.data.weather[0].description] || response.data.weather[0].description
 
             const weatherData = {
                 Temperatura: response.data.main.temp,
@@ -37,10 +50,10 @@ app.get("/climatempo/:cidade", async (req, res) => {
 
             res.send(weatherData);
         } else {
-            res.status(response.status).send({erro: 'Erro ao obter dados meteoro'});
+            res.status(response.status).send({ erro: 'Erro ao obter dados meteoro' });
         }
-    } catch (error){
-        res.status(500).send({erro:'Erro ao obter dados meterológicos', error});
+    } catch (error) {
+        res.status(500).send({ erro: 'Erro ao obter dados meterológicos', error });
     }
 
 });
